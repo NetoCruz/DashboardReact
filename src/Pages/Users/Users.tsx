7f11cc106a848f4aacb0ff2d18c8a6094bb48c79
Neto Cruz <galaxy.net0710@gmail.com>
2024-03-31 13:52:48 -0600

    Users table added

--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -1,7 +1,14 @@
+import DataTable from '../../Components/DataTable/DataTable'
 import './users.scss'
 const Users =() =>{
  return(
-    <div className="users">Users</div>
+    <div className="users">
+      <div className="info">
+         <h1>Users</h1>
+         <button>Add User</button>
+      </div>
+      <DataTable/>
+    </div>
  )
 }
 
